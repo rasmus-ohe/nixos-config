f686b24422761c35fbb191eb7ecf9a11d7b226d3
rasmus-ohe <rassemichael@gmail.com>
2026-04-13 18:50:15 +0300

    Static ip

--- hosts/kvm/configuration.nix
+++ hosts/kvm/configuration.nix
@@ -28,6 +28,16 @@
       # allowedTCPPorts = [ ... ];
       # allowedUDPPorts = [ ... ];
     };
+    
+    # Static QEMU network
+    interfaces.enp1s0 = {
+      ipv4.addresses = [{
+	address = "192.168.122.200";
+	prefixLength = 24;
+      }];
+    };
+    defaultGateway = "192.168.122.1";
+    nameservers = [ "1.1.1.1" "8.8.8.8" ];
   };
 
   # Set your time zone.
